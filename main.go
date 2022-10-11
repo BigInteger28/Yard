902cc9ff3357820789df62e6806fa35fa219f2bc
Joris Verstegen <39861914+BigInteger28@users.noreply.github.com>
2022-10-11 22:26:26 +0200

    1.1

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 	"strings"
 )
 
-var BlokIndex = []string{"E", "KCA", "KCB", "PL", "J", "L", "N", "R", "Q2", "K408", "K409", "LOODSC", "HLN"}
+var BlokIndex = []string{"E", "F", "H", "KCA", "KD", "KCB", "PL", "J", "L", "N", "R", "Q2", "K408", "K409", "LOODSC", "HLN"}
 var Lijnplaatsen = [][]int{
 	/*E*/ {19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 0, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 15, 9, 2},
+	/*F*/ {20, 21, 22, 22, 21, 21, 21, 21, 21, 21, 19, 19, 18, 16, 17, 22, 22, 18, 18, 18},
+	/*H*/ {26, 26, 26, 26, 26, 25, 24, 25, 25, 25, 25, 25, 25, 25, 22, 22, 22, 22, 22, 22},
 	/*KCA*/ {20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 14, 14, 14, 14, 14, 14},
+	/*KD*/ {29, 29, 29, 29, 29},
 	/*KCB*/ {20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 14, 14, 14, 14, 14, 14},
 	/*PL*/ {21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21},
 	/*J*/ {27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27},
@@ -20,8 +23,8 @@ var Lijnplaatsen = [][]int{
 	/*Q2*/ {20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20},
 	/*K408*/ {39, 39, 39, 39, 39, 39},
 	/*K409*/ {49, 49, 49, 49, 49, 49},
-	/*LOODSC*/ {240},
-	/*HLN*/ {},
+	/*LOODSC*/ {10, 10, 10, 10, 10, 10, 10, 10, 0, 6, 12, 12, 12, 12, 12, 12, 12, 12, 12, 10, 10, 10},
+	/*HLN*/ {15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15},
 }
 
 // lengtecat --> veryshort/short/normal/long/verylong
@@ -32,16 +35,10 @@ type Auto struct {
 	hoogte    float64
 }
 
-func geefPlaatsenOpBlok(blok string, startlijn int, eindlijn int) [4]int {
+func geefPlaatsenOpBlok(blok string, startlijn int, eindlijn int, eigenlengte float64) [4]int {
 	var blokindex int
 	var totaalPlaatsen [4]int
-	var vermenigvuldiger = [4]float64{1, 1.136, 0.877, 1.85}
-	/*
-		Normaal 	5m/slot
-		Korte		4.4m/slot
-		Lange		5.7m/slot
-		Heel korte	2.7m/slot
-	*/
+	var vermenigvuldiger = [4]float64{1, 1.114, 0.86, 1.814}
 	for s := range BlokIndex {
 		if BlokIndex[s] == strings.ToUpper(blok) {
 			blokindex = s
@@ -49,9 +46,14 @@ func geefPlaatsenOpBlok(blok string, startlijn int, eindlijn int) [4]int {
 		}
 	}
 	for i := startlijn; i <= eindlijn; i++ {
-		for v := 0; v < 4; v++ {
-			var plaatsen float64 = vermenigvuldiger[v] * float64(Lijnplaatsen[blokindex][i-1])
-			totaalPlaatsen[v] += int(math.Round(plaatsen))
+		if eigenlengte == 0 {
+			for v := range vermenigvuldiger {
+				var plaatsen float64 = vermenigvuldiger[v] * float64(Lijnplaatsen[blokindex][i-1])
+				totaalPlaatsen[v] += int(math.Round(plaatsen))
+			}
+		} else {
+			var plaatsen float64 = (4.9 / eigenlengte) * float64(Lijnplaatsen[blokindex][i-1])
+			totaalPlaatsen[0] += int(math.Round(plaatsen))
 		}
 	}
 	return totaalPlaatsen
@@ -60,8 +62,9 @@ func geefPlaatsenOpBlok(blok string, startlijn int, eindlijn int) [4]int {
 func main() {
 	for {
 		var keuze int
-		fmt.Println("1. Kies kaai")
+		fmt.Println("\n1. Kies kaai")
 		fmt.Println("2. Aantal plaats voor blok")
+		fmt.Println("3. Overzicht aantal lijnen per blok")
 		fmt.Print("Keuze: ")
 		fmt.Scanln(&keuze)
 
@@ -70,7 +73,10 @@ func main() {
 		} else if keuze == 2 {
 			var start string
 			var blok string
+			var eigenlengte float64
 			var tot [4]int
+			fmt.Print("Eigen lengte (0 is geen eigen lengte): ")
+			fmt.Scanln(&eigenlengte)
 			fmt.Print("Blok: ")
 			fmt.Scanln(&blok)
 			for start != "." {
@@ -81,17 +87,32 @@ func main() {
 				if start != "." {
 					fmt.Print("Stop lijn: ")
 					fmt.Scanln(&eindln)
-					totL := geefPlaatsenOpBlok(blok, startln, eindln)
+					var totL [4]int
+					if eigenlengte > 0 {
+						totL = geefPlaatsenOpBlok(blok, startln, eindln, eigenlengte)
+					} else {
+						totL = geefPlaatsenOpBlok(blok, startln, eindln, 0)
+					}
 					for leng := 0; leng < 4; leng++ {
 						tot[leng] += totL[leng]
 					}
 				}
 			}
-			fmt.Println("Op blok ", strings.ToUpper(blok), ":")
-			fmt.Println("voor VOLVO_XC60 / MG_5 auto's zijn er ", tot[0], " plaatsen")
-			fmt.Println("voor PEUGEOT_208 auto's zijn er ", tot[1], " plaatsen")
-			fmt.Println("voor FORD_RANGER auto's zijn er ", tot[2], " plaatsen")
-			fmt.Println("voor CITROEN_AMI auto's zijn er ", tot[3], " plaatsen")
+			if eigenlengte > 0 {
+				fmt.Println("\nOp blok ", strings.ToUpper(blok), ":")
+				el := fmt.Sprintf("%.1f", eigenlengte)
+				fmt.Println("voor auto's (", el, "m ) zijn er ", tot[0], " plaatsen")
+			} else {
+				fmt.Println("\nOp blok ", strings.ToUpper(blok), ":")
+				fmt.Println("voor MG_5 auto's (4.9m) zijn er ", tot[0], " plaatsen")
+				fmt.Println("voor PEUGEOT_208 (4.4m) auto's zijn er ", tot[1], " plaatsen")
+				fmt.Println("voor FORD_RANGER (5.7m) auto's zijn er ", tot[2], " plaatsen")
+				fmt.Println("voor CITROEN_AMI (2.7m) auto's zijn er ", tot[3], " plaatsen")
+			}
+		} else if keuze == 3 {
+			for i := range BlokIndex {
+				fmt.Println(BlokIndex[i], ":  ", len(Lijnplaatsen[i]), " lijnen")
+			}
 		}
 	}
 }
