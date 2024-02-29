9ae8ee74498943e023940589a29823992686eb25
Joris Verstegen <39861914+BigInteger28@users.noreply.github.com>
2024-02-29 15:15:07 +0100

    29.02.2024

--- runcommands.go
+++ runcommands.go
@@ -26,6 +26,7 @@ func plakInput(inputTxt string) []string {
 func main() {
 	for {
 		var keuze int
+		var vins []string
 		fmt.Println("\n0. AANPASSEN X VAN VIN")
 		fmt.Println("1. HH MODEL naar TOS MODEL")
 		fmt.Println("2  FINISH AFO")
@@ -38,11 +39,12 @@ func main() {
 		fmt.Println("9. Wijzig positie meerdere vins")
 		fmt.Println("10. Inventory Check")
 		fmt.Println("11. Vins opladen onder laatste 7 of 8")
-		fmt.Println("")
+		fmt.Println("12. CATEGORY 2 aanpassen")
+		fmt.Println("13. ReadyForTransport/ReadyForPickup")
+		fmt.Println("14. Next Pos aanpassen")
 		fmt.Print("Keuze: ")
 		fmt.Scanln(&keuze)
 		if keuze == 0 {
-			var vins []string
 			var item string
 			var waarde string
 			fmt.Print("Item dat je wil aanpassen (bv weight): ")
@@ -88,7 +90,6 @@ func main() {
 				fmt.Print("finishafo, afo ", ordernr[i], "\n")
 			}
 		} else if keuze == 3 {
-			var vins []string
 			var dtsleft string
 			var hrleft string
 			fmt.Print("Datum left (dd.mm.yyyy): ")
@@ -100,7 +101,6 @@ func main() {
 				fmt.Print("vin_edit ", vins[i], ", dts_left ", dtsleft, " ", hrleft, "\n")
 			}
 		} else if keuze == 4 {
-			var vins []string
 			var visit string
 			fmt.Print("Visit: ")
 			fmt.Scanln(&visit)
@@ -110,7 +110,6 @@ func main() {
 				fmt.Print("ArrivedScan, vin ", vins[i], ", visit ", visit, "\n")
 			}
 		} else if keuze == 5 {
-			var vins []string
 			var position string
 			var slot int
 			fmt.Print("Scan op positie: ")
@@ -149,7 +148,6 @@ func main() {
 			} else {
 				printer = "PTOHAN08"
 			}
-			var vins []string
 			vins = plakInput("vins")
 			for i := range vins {
 				fmt.Print("PrintCargoLabelexecution,vin ", vins[i], ", printerName ", printer,"\n")
@@ -171,7 +169,6 @@ func main() {
 				fmt.Print("BlockPosition, position", positions[i], ",start_time", starttijd, ",end_time", eindtijd,"\n")
 			}
 		} else if keuze == 9 {
-			var vins []string
 			var position string
 			var startS int
 			var vis bool
@@ -193,8 +190,7 @@ func main() {
 					fmt.Print("vin_edit ", vins[i], ", Position ", position, ", SLOT ", startS+i, "\n")
 				}
 			}			
-		} else if keuze == 10 {
-			var vins []string
+		} else if keuze == 10 {			
 			var check string
 			fmt.Print("Inventory Check (TRUE/FALSE): ")
 			fmt.Scanln(&check)
@@ -204,13 +200,61 @@ func main() {
 				fmt.Print("vin_edit ", vins[i], ", Inventory_Check ", check, "\n")
 			}
 		} else if keuze == 11 {
-			var vins []string
 			var laatste int
 			fmt.Print("Onder laatste: ")
 			fmt.Scanln(&laatste)
 			vins = plakInput("vins")
 			for i := range vins {
 				fmt.Print("vin_edit ", vins[i], ", additional_id ", vins[i][len(vins[i])-laatste:], "\n")
+			}
+		} else if keuze == 12 {
+			var waarde string
+			fmt.Print("Waarde: ")
+			fmt.Scanln(&waarde)
+			vins = plakInput("vins")			
+			for i := range vins {
+				fmt.Print("vin_edit ", vins[i], ", CATEGORY_2 ", waarde, "\n")
+			}
+		} else if keuze == 13 {
+			var menu int
+			var sbool, sbool2 string
+			fmt.Println("1. RFT")
+			fmt.Println("2. RFP")
+			fmt.Println("3. Beide")
+			fmt.Print("Keuze: ")
+			fmt.Scanln(&menu)
+			vins = plakInput("vins")
+			if menu == 1 {
+				fmt.Print("RFT (true / false): ")
+				fmt.Scanln(&sbool)
+				for i := range vins {
+					fmt.Print("SetRFT ,vin ", vins[i], ", remove ", sbool, "\n")
+				}
+			} else if menu == 2 {
+				fmt.Print("RFP (true / false): ")
+				fmt.Scanln(&sbool)
+				for i := range vins {
+					fmt.Print("SetRFP ,vin ", vins[i], ", remove ", sbool, "\n")
+				}
+
+			} else {
+				fmt.Print("RFT (true / false): ")
+				fmt.Scanln(&sbool)
+				fmt.Print("RFP (true / false): ")
+				fmt.Scanln(&sbool2)
+				for i := range vins {
+					fmt.Print("SetRFT ,vin ", vins[i], ", remove ", sbool, "\n")
+					fmt.Print("SetRFP ,vin ", vins[i], ", remove ", sbool2, "\n")
+				}
+			}
+		} else if keuze == 14 {
+			var nextpos string
+			fmt.Print("Next Pos: ")
+			fmt.Scanln(&nextpos)
+			nextpos = strings.ToUpper(nextpos)
+			vins = plakInput("vins")
+			for i := range vins {
+				fmt.Print("vin_edit ", vins[i], ", Next_Pos ", nextpos, "\n")
 			}
 		}
 	}
